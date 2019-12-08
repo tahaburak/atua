94c88c8997ebd47b4b8427b63d83c68e59dcefa0
Taha Burak <tahaburak.koc@gmail.com>
2019-12-08 23:22:33 +0300

    initial commit

---
+++ src/main/java/com/tahaburak/atua/AtuaApplication.java
@@ -0,0 +1,13 @@
+package com.tahaburak.atua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtuaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtuaApplication.class, args);
+	}
+
+}
