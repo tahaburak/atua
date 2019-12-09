4ad6b128602aeae0f560557649dc9e25c3bf0a3f
Taha Burak <tahaburak.koc@gmail.com>
2019-12-09 03:30:24 +0300

    implemented mail and slack notifications

--- src/main/java/com/tahaburak/atua/AtuaApplication.java
+++ src/main/java/com/tahaburak/atua/AtuaApplication.java
@@ -1,8 +1,12 @@
 package com.tahaburak.atua;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@EnableAdminServer
 @SpringBootApplication
 public class AtuaApplication {
 
